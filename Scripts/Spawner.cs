d48711dc4e535e352a20c8d112a67a9916ff97fc
Ryan G <ryan.goh.2022@scis.smu.edu.sg>
2024-04-01 15:20:36 +0800

    lots of fixes

--- Scripts/Spawner.cs
+++ Scripts/Spawner.cs
@@ -3,10 +3,32 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject prefabToInstantiate; // Reference to the prefab you want to instantiate
+    private bool isSpawned = false;
 
-    void Start()
+    void Update()
     {
-        // Instantiate the prefab at a specific position and rotation
-        Instantiate(prefabToInstantiate, transform.position, Quaternion.identity);
+        if (!isSpawned)
+        {
+            // Instantiate the prefab
+            GameObject instantiatedPrefab = prefabToInstantiate;
+
+            // Ensure that the instantiated object has a Rigidbody component
+            Rigidbody rb = instantiatedPrefab.GetComponent<Rigidbody>();
+            if (rb == null)
+            {
+                Debug.Log("no rigid");
+                rb = instantiatedPrefab.AddComponent<Rigidbody>();
+            }
+
+            // Ensure that the instantiated object has a Collider component
+            Collider col = instantiatedPrefab.GetComponent<Collider>();
+            if (col == null)
+            {
+                Debug.Log("no collider");
+                col = instantiatedPrefab.AddComponent<MeshCollider>(); // You can replace MeshCollider with the appropriate collider type
+            }
+            Instantiate(instantiatedPrefab, transform.position, Quaternion.identity);
+            isSpawned = true;
+        }
     }
 }
--- Scripts/UnlockDoor.cs
+++ Scripts/UnlockDoor.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class UnlockDoor : MonoBehaviour
 {
     public GameObject toDestroy;
+    public bool requiresDragonKey;
     void OnTriggerEnter(Collider other){
-        if (other.CompareTag("Giant Key") || other.CompareTag("Dragon Key"))
+        if ((other.CompareTag("Giant Key") && !requiresDragonKey) || (other.CompareTag("Dragon Key") && requiresDragonKey))
         {
             AudioManager.instance.PlayUnlockSound();
             Destroy(toDestroy);
